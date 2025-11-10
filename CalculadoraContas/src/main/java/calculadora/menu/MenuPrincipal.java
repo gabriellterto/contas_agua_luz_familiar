08fe70e88d70c5e3ec126b7887b4f4d2b7bb5722
Gabriel Terto <gabrielterto1002@gmail.com>
2025-11-09 21:56:51 -0300

    feat: Menu principal

--- CalculadoraContas/src/main/java/calculadora/menu/MenuPrincipal.java
+++ CalculadoraContas/src/main/java/calculadora/menu/MenuPrincipal.java
@@ -1,5 +1,19 @@
 package calculadora.menu;
+import java.util.Scanner;
+
 
 public class MenuPrincipal {
-    
+    public void exibirMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("-".repeat(30));
+        System.out.println("MENU PRINCIPAL");
+        System.out.println("-".repeat(30));
+        System.out.println("1 - Calculadora de Água");
+        System.out.println("2 - Calculadora de Luz");
+        System.out.println("0 - Sair");
+        System.out.println("-".repeat(30));
+        System.out.print("Escolha: ");
+        int escolha = scanner.nextInt();
+    }
 }
