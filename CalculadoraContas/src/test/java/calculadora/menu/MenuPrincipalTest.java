77af673743cde2bfd016f6711bcf0f7aae20fe03
Gabriel Terto <gabrielterto1002@gmail.com>
2025-11-20 17:21:14 -0300

    feat: Comentario nas classes de teste para apenas colocar os arquivos de classe de teste como UTF-8 pra passar na esteira

--- CalculadoraContas/src/test/java/calculadora/menu/MenuPrincipalTest.java
+++ CalculadoraContas/src/test/java/calculadora/menu/MenuPrincipalTest.java
@@ -1,8 +1,9 @@
-﻿package calculadora.menu;
+package calculadora.menu;
 
 import org.junit.jupiter.api.Test;
 
 public class MenuPrincipalTest {
+    // Teste para o método exibirMenu
     @Test
     public void testExibirMenu() {
         MenuPrincipal menu = new MenuPrincipal();
